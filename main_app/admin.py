68ba62779daf9c62bd752466c38efd170d29612c
metadata <metadata@rajkumar>
2018-04-03 11:01:59 +0530

    mini

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 
 # Register your models here.
-from main_app.models import ClientRatings, WorkerRatings
 
-admin.site.register(ClientRatings)
-admin.site.register(WorkerRatings)
