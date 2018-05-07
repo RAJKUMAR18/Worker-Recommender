a025e18eb024b0ca72cfbdbe21338751f0872aec
RAJKUMAR18 <princegosavi12@gmail.com>
2018-05-07 15:23:22 +0530

    Added extra info field Phone number

--- main_app/utils.py
+++ main_app/utils.py
@@ -11,7 +11,7 @@ import pickle
 from sklearn.neighbors import NearestNeighbors
 from sklearn.cluster import KMeans
 # import os
-
+from datetime import datetime
 def vectorizer(columnsValues):
     # location,service,certification,age_prefernce,gender,types,availability,
     #           wage_preference,exprience,clients_attended,doorstep_service,
@@ -20,13 +20,13 @@ def vectorizer(columnsValues):
     location = [0] * 44
     occupatn = [0] * 23
     cert = [0] * 2
-    age = [0] * 5
+    age = [0] * 4
     gender = [0] * 2
     types = [0] * 2
     availability = [0] * 4
     minimumWage = [0] * 3
     exp = [0] * 3
-    clients = [0] * 3
+    clients = [0] * 5
     references = [0] * 2
     liscenced = [0] * 2
     shoppingliscence = [0] * 2
@@ -64,34 +64,45 @@ def vectorizer(columnsValues):
     return list(vector)
 
 
-
 class BuildAndTrain():
 
     def __init__(self):
+        """Calls dataUtility utitlities functions"""
+        self.start_time = datetime.now()
         self.df = self.dataUtility()
         self.classesOfColumns = defaultdict(list)
         self.occupations = defaultdict(list)
         self.kmeans = []
         self.df = self.utilities(self.df)
+        # print('utilities called!!')
         # self.kneighborsOfUserQuery, self.finalCluster = self.KmeanPredictor('1', sparse1[116])
         self.classesOfColumns = self.unpickleLoader('clsofclos')
         self.occupations = self.unpickleLoader('occupations')
+        # print(self.occupations.keys())
+    
+    
+    def pickler(self, toBeDumped, filename):
+        """A helper function to pickle data"""
+        with open(str(filename) + '.pkl', 'wb') as file:
+            file.write(pickle.dumps(toBeDumped))
 
-##################################### DATA UTILITY ###################################################
-
+    def unpickleLoader(self,filename):
+        """A helper function to unpickle data"""
+        with open(filename + '.pkl', 'rb') as f:
+            unpickled = pickle.loads(f.read())
+        return unpickled
+    
     def dataUtility(self):
+        """Reads the main input csv in a dataframe for computation"""
         df = pd.read_csv('final_data.csv')
-        df = df.drop(['phoneNo', 'id', 'availabilityPreference', 'aadharCard'],
+        df = df.drop(['id', 'availabilityPreference', 'aadharCard'],
                      axis=1)
         df.dropna(inplace=True)
-        print('DataUtility Done')
+        # print('DataUtility Done')
         return df
 
-#################################### UTILITY FUNCTIONS IMPLEMENTED######################################
-
     def classer(self, temp_df):
-    # np.logical_and(df[:]['Location'] == 30, df[:]['Occupation'] ==2).nonzero()[0].tolist()
-
+        """Groups age, experience, clientsAttended information"""
         temp_df.loc[temp_df['minimumWage']<5001, 'minimumWage'] = 0
         temp_df.loc[np.logical_and(temp_df['minimumWage']>5000, temp_df['minimumWage']<8001),'minimumWage'] = 1
         temp_df.loc[np.logical_and(temp_df['minimumWage']>8000, temp_df['minimumWage']<10001),'minimumWage'] = 2
@@ -111,53 +122,64 @@ class BuildAndTrain():
         temp_df.loc[np.logical_and(temp_df['clientsAttended']>30, temp_df['clientsAttended']<41),'clientsAttended'] = 3
         temp_df.loc[temp_df['clientsAttended']>40, 'clientsAttended'] = 4
         return temp_df
-
+    
     def classes_maker(self,temp_df):
-        for i in temp_df.columns:
+        """Label encoding for all non numeric data and returns new df"""
+        temp = temp_df.columns.tolist()
+        temp.remove('phoneNo')
+        for i in temp:
             le = preprocessing.LabelEncoder()
             le.fit(temp_df[i])
             self.classesOfColumns[i].append(le.classes_)
             temp_df[i] = le.transform(temp_df[i])
+        # print(temp_df.columns)
         return temp_df
-
+    
     def all_occupations_in_a_location(self, temp_df):
-    # location: index and occupation where index is the position of row in
-    # Dataframe
-    # key: occupation lst: list of worker having occupation (index, location)
+        """Finds all the workers at all locations and store it in dict with key as\
+        occupation and value as list of indexes"""
+        # print('Sorting workers')
         for index, row in temp_df.iterrows():
             self.occupations[row['occupation']].append(index)
 
         for key, values in self.occupations.items():
             t_set = list(set(values))
             self.occupations[key] = t_set
-
+        # print(self.occupations.keys())
+        
     def occs_splitter(self, df):
+        """Splits data into multiple datasets w.r.t occupation and stores it in a  seperate\
+            csv file"""
+        # print('Splitting data.....')
         for key in self.occupations.keys():
             temp_df = df.iloc[self.occupations[key]]
             # temp_df.loc[:, ~df.columns.str.contains('^Unnamed')]
             temp_df.to_csv(str(key) + '.csv', index=False)
-
+    
+    
     def sparser(self):
+        """Generate sparse matrix of the splitted data and pickles the matrix"""
+        # print('Generating sparse matrix for data...')
         for i in range(len(self.occupations.keys())):
             sparse = []
-            temp_df = pd.read_csv(str(i) + '.csv')
+            temp_df = pd.read_csv(str(i)+'.csv')
 
             for index, row in temp_df.iterrows():
-                vector = []
-                location = [0] * np.unique(self.df['location'])
-                occupatn = [0] * np.unique(self.df['occupation'])
-                cert = [0] * np.unique(self.df['certification'])
-                age = [0] * np.unique(self.df['age'])
-                gender = [0] * np.unique(self.df['gender'])
-                types = [0] * np.unique(self.df['type'])
-                availability = [0] * np.unique(self.df['availability'])
-                minimumWage = [0] * np.unique(self.df['minimumWage'])
-                exp = [0] * np.unique(self.df['experience'])
-                clients = [0] * np.unique(self.df['clientsAttended'])
-                references = [0] * np.unique(self.df['references'])
-                liscenced = [0] * np.unique(self.df['liscenced'])
+                vector           = []
+                location         = [0] * np.unique(self.df['location'])
+                occupatn         = [0] * np.unique(self.df['occupation'])
+                cert             = [0] * np.unique(self.df['certification'])
+                age              = [0] * np.unique(self.df['age'])
+                gender           = [0] * np.unique(self.df['gender'])
+                types            = [0] * np.unique(self.df['type'])
+                availability     = [0] * np.unique(self.df['availability'])
+                minimumWage      = [0] * np.unique(self.df['minimumWage'])
+                exp              = [0] * np.unique(self.df['experience'])
+                clients          = [0] * np.unique(self.df['clientsAttended'])
+                references       = [0] * np.unique(self.df['references'])
+                liscenced        = [0] * np.unique(self.df['liscenced'])
                 shoppingliscence = [0] * np.unique(self.df['shoppingliscence'])
-                doorstepService = [0] * np.unique(self.df['doorstepService '])
+                doorstepService  = [0] * np.unique(self.df['doorstepService '])
 
                 location[row['location']] = 1
                 occupatn[row['occupation']] = 1
@@ -182,7 +204,7 @@ class BuildAndTrain():
                 vector.extend(types)
                 vector.extend(availability)
                 vector.extend(minimumWage)
-                # vector.extend(exp)
+                vector.extend(exp)
                 vector.extend(clients)
                 vector.extend(doorstepService)
                 vector.extend(references)
@@ -190,48 +212,42 @@ class BuildAndTrain():
                 vector.extend(shoppingliscence)
                 sparse.append(list(vector))
             self.pickler(sparse, str(i)+'_sparse')
-
+    
     def utilities(self, temp_df):
-        temp_df = self.classer(temp_df)
-        temp_df = self.classes_maker(temp_df)
+        """Calls multiple utilities and return the result dataframe"""
+        # print('Executing utilities functions ....')
+        # temp_df = self.classer(temp_df)
+        # temp_df = self.classes_maker(temp_df)
         # self.all_occupations_in_a_location(temp_df)
-        self.occs_splitter(temp_df)
-        self.sparser()
+        # self.occs_splitter(temp_df)
+        # self.sparser()
         # self.pickler(self.classesOfColumns, 'clsofclos')
         # self.pickler(self.occupations, 'occupations')
-        print("Utilites executed")
+        # print("Utilites executed")
         return temp_df
-
-################################GENERIC FUNCTIONS#####################################
-
-    def pickler(self, toBeDumped, filename):
-        with open(str(filename) + '.pkl', 'wb') as file:
-            file.write(pickle.dumps(toBeDumped))
-
-    def unpickleLoader(self,filename):
-        with open(filename + '.pkl', 'rb') as f:
-            unpickled = pickle.loads(f.read())
-        return unpickled
-
-##############MODELLING STUFF##########################################
-
+    
     def modelling(self, service, userquery):
-        # read the sparse matrix file of the particular job example
+        """Creates a Kmean model and starts ml processes in cascade"""
+        # print('Generating model ...')
         temp_files = []
         for i in range(len(self.occupations.keys())):
             temp_files.append(self.unpickleLoader(str(i)+'_sparse'))
             kmodel = KMeans(max_iter=4,
                             n_clusters=10, n_init=10).fit(temp_files[i])
             self.kmeans.append(kmodel)
-        print('Modelling done')
+            # self.pickler(kmodel, str(i) + '_model')
+        # print('Modelling done')
         return self.KmeanPredictor(service, userquery)
-
+    
     def KmeanPredictor(self,service, userquery): # modelNos same as service
+        """Predicts the cluster in which user query belongs to"""
         kmeanModel = self.unpickleLoader(str(service) + '_model')
-        print('Predicting kmean cluster')
+        # print('Predicting kmean cluster')
         return self.KMeanClusterIndexes(kmeanModel, kmeanModel.predict(np.array(userquery).reshape(1,-1)), userquery, service)
-
+    
+    
     def KMeanClusterIndexes(self, kMeanModel, userQueryClusterLabel, userquery, service):
+        """Get all the data points in the user query cluster"""
         temp = kMeanModel.labels_.tolist()
         count = 0
         li = []
@@ -239,22 +255,26 @@ class BuildAndTrain():
             if i == userQueryClusterLabel:
                 li.append(count)
             count = count + 1
-        print('getting all points in the same cluster')
+        # print('getting all points in the same cluster')
         return self.clusteredDataframe(li, service, userquery)
-
+    
     def clusteredDataframe(self, clustEleIndex, service, userQuery):
+        """Process the data in the clustered dataframe"""
         temp_sparse = self.unpickleLoader(str(service) + '_sparse')
         temp_df = pd.read_csv(str(service) + '.csv')
         KMclustered_dataframe = temp_df.loc[clustEleIndex]
         temp_sparse = [temp_sparse[x] for x in clustEleIndex]
-        print('Temporary cluster formation')
+        # print('Temporary cluster formation')
         return self.NearestNeighborsAlgo(temp_sparse, userQuery,KMclustered_dataframe)
-
+    
     def NearestNeighborsAlgo(self, clusteredSparse, userQuery, KMeanClusterIndexes):
+        """Apply KNN to the clustered dataframe"""
         neigh = NearestNeighbors(n_neighbors=15)
         neigh.fit(clusteredSparse)
-        print('Applying nearest neighbour')
+        # print('Applying nearest neighbour')
+        print("Total time: ", datetime.now() - self.start_time)
         return neigh.kneighbors(np.array(userQuery).reshape(1,-1)), KMeanClusterIndexes
+
 
 # kmeans = []
 # if __name__ == '__main__':
--- main_app/views.py
+++ main_app/views.py
@@ -33,7 +33,7 @@ def result(request):
         if query_data.is_valid():
             bnt = BuildAndTrain()
             classes = bnt.unpickleLoader('clsofclos')
-            print(classes)
+            # print(classes)
             print('form data validated')
             name = query_data.cleaned_data['your_name']
             mob_number = query_data.cleaned_data['mob_number']
@@ -53,7 +53,6 @@ def result(request):
             shopping_liscence = random.randint(0, 1)
 
             print('context generated')
-            print('building model')
             
             context = {
                 'name': name, 'mob_number': mob_number,
@@ -78,34 +77,41 @@ def result(request):
 
             print('done')
             
-            location = str(classes['location'][0][int(location)])
-            shopping_liscence = str(classes['shoppingliscence'][0][int(shopping_liscence)])
-            liscenced = str(classes['liscenced'][0][int(liscenced)])
-            reference = str(classes['references'][0][int(reference)])
-            doorstep_service = str(classes['doorstepService '][0][int(doorstep_service)])
-            clients_attended = str(classes['clientsAttended'][0][int(clients_attended)])
-            service = str(classes['occupation'][0][int(service)])
-            certification = str(classes['certification'][0][int(certification)])
-            types = str(classes['type'][0][int(types)])
-            age_prefernce = str(classes['age'][0][int(age_prefernce)])
-            wage_preference = str(classes['minimumWage'][0][int(wage_preference)])
-            availability = str(classes['availability'][0][int(availability)])
-            gender = str(classes['gender'][0][int(gender)])
-            experience = str(classes['experience'][0][int(experience)])
+            # location = str(classes['location'][0][int(location)])
+            # shopping_liscence = str(classes['shoppingliscence'][0][int(shopping_liscence)])
+            # liscenced = str(classes['liscenced'][0][int(liscenced)])
+            # reference = str(classes['references'][0][int(reference)])
+            # doorstep_service = str(classes['doorstepService '][0][int(doorstep_service)])
+            # clients_attended = str(classes['clientsAttended'][0][int(clients_attended)])
+            # service = str(classes['occupation'][0][int(service)])
+            # certification = str(classes['certification'][0][int(certification)])
+            # types = str(classes['type'][0][int(types)])
+            # age_prefernce = str(classes['age'][0][int(age_prefernce)])
+            # wage_preference = str(classes['minimumWage'][0][int(wage_preference)])
+            # availability = str(classes['availability'][0][int(availability)])
+            # gender = str(classes['gender'][0][int(gender)])
+            # experience = str(classes['experience'][0][int(experience)])
 
-            context = {
-                'name': name, 'mob_number': mob_number,
-                'location': location, 'service': service,
-                'certification': certification, 'types': types,
-                'age_prefernce': age_prefernce, 'gender': gender,
-                'availability': availability,
-                'wage_preference': wage_preference,
-                'experience': experience, 'clients_attended': clients_attended,
-                'doorstep_service': doorstep_service, 'reference': reference,
-                'liscenced': liscenced, 'shopping_liscence': shopping_liscence,
-            }
+            # context = {
+            #     'name': name, 'mob_number': mob_number,
+            #     'location': location, 'service': service,
+            #     'certification': certification, 'types': types,
+            #     'age_prefernce': age_prefernce, 'gender': gender,
+            #     'availability': availability,
+            #     'wage_preference': wage_preference,
+            #     'experience': experience, 'clients_attended': clients_attended,
+            #     'doorstep_service': doorstep_service, 'reference': reference,
+            #     'liscenced': liscenced, 'shopping_liscence': shopping_liscence,
+            # }
+            temp_df = pd.read_csv(str(service) + '.csv')
             context['clusterDF'] = None
-            context['clusterDF'] = finalCluster.iloc[kneighborsOfUserQuery[1][0]].to_html()
+            # context['clusterDF'] = finalCluster.iloc[kneighborsOfUserQuery[1][0]].to_html()
+            
+            combined = temp_df.append(finalCluster.iloc[kneighborsOfUserQuery[1][0]])
+            combined[~combined.index.duplicated(keep=False)]
+            final = finalCluster.iloc[kneighborsOfUserQuery[1][0]].append(combined)
+            
+            context['clusterDF'] = final.to_html()
 
             return render(request, 'result.html', context=context)
         else:
